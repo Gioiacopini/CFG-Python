39725b4d9161fbdb0f6861849bfe0e95b2e1c884
giacopini <gio.iacopini@gmail.com>
2018-02-14 19:46:49 +0000

    comments added

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#import flask from flask library
 from flask import Flask
 
+#use flask() to create an app
 app = Flask("my_first_app")
 
+#this line makes the function capable of responding to requests
 @app.route("/")
 def say_hello():
     return "Hello World"
