f0dd8b7f11ad5e1644e1bb5ae7f17a9301500a4c
giacopini <gio.iacopini@gmail.com>
2018-02-14 20:03:17 +0000

    css

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #import flask from flask library
-from flask import Flask
+from flask import Flask, render_template
+
 
 #use flask() to create an app
 app = Flask("my_first_app")
 
 #this line makes the function capable of responding to requests
-@app.route("/")
+@app.route("/qwerty")
 def say_hello():
-    return "Hello World"
+    return render_template("index.html")
 
 app.run(debug=True, host="0.0.0.0")
